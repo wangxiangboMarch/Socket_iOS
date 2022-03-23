e1f5e7f5a01a7db26f70a0bc17dc7cfb759f51f0
wangxiangbo <laoyouhub@icloud.com>
2022-03-23 18:12:40 +0800

    添加客户端

---
+++ SocketClient/SocketClient/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SocketClient
+//
+//  Created by GongsiWang on 2022/3/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
